fc1d55d160b5c8dffeb23389eec75c4bca026008
anapernalete <pernaleteanaflor@gmail.com>
2025-04-22 21:48:40 -0400

    endpoint

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi import Query
 import pandas as pd
 import sqlite3
+import joblib
 
 if not os.path.exists("ecommerce.db"):
     df = pd.read_csv("data.csv", encoding="ISO-8859-1")
@@ -46,6 +47,46 @@ def top_products(limit: int = 10):
     with open("queries/top_products.sql") as f:
         query = f.read()
     return {"data": run_query(query, (limit))}
+
+@app.get("/customer-segment")
+def get_customer_segment(customer_id: int = Query(...)):
+    kmeans_model = joblib.load("models/kmeans_model.pkl")
+    scaler = joblib.load("models/rfm_scaler.pkl")
+    
+    #Load transactions
+    conn = sqlite3.connect("ecommerce.db")
+    df = pd.read_sql_query("SELECT * FROM transactions", conn)
+    conn.close()
+    
+    df = df.dropna(subset=["CustomerID", "InvoiceDate", "InvoiceNo", "Quantity", "UnitPrice"])
+    df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
+    
+    if customer_id not in df["CustomerID"].unique():
+        return {"error": f"Customer ID {customer_id} not found"}
+    
+    latest_date = df["InvoiceDate"].max()
+    cust_df = df[df["CustomerID"] == customer_id]
+    
+    recency = (latest_date - cust_df["InvoiceDate"].max()).days
+    frequency = cust_df["InvoiceNo"].nunique()
+    monetary = (cust_df["Quantity"] * cust_df["UnitPrice"]).sum()
+    
+    rfm_features = pd.DataFrame([{
+        "Recency": recency,
+        "Frequency": frequency,
+        "Monetary": monetary
+    }])
+    
+    rfm_scaled = scaler.transform(rfm_features)
+    segment = int(kmeans_model.predict(rfm_scaled)[0])
+    
+    return {
+        "customer_id": customer_id,
+        "recency": recency,
+        "frequency": frequency,
+        "monetary": round(monetary, 2),
+        "segment": segment
+    }
 
 if __name__ == "__main__":
     import uvicorn
