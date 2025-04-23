5f4b7592dd85ad7ded18fd066b8e67088b3bc3b1
anapernalete <pernaleteanaflor@gmail.com>
2025-04-22 23:21:14 -0400

    add customer segmentation to streamlit adashboard and finalize API integration

--- streamlit-dashboard/app.py
+++ streamlit-dashboard/app.py
@@ -7,7 +7,12 @@ API_URL = "https://ecommerce-analytics-api.onrender.com"
 st.title("E-commerce Dashboard")
 
 # Sidebar
-option = st.sidebar.selectbox("Select a view", ["Top Customers", "Daily Sales"])
+option = st.sidebar.selectbox("Select a view", [
+    "Top Customers",
+    "Daily Sales",
+    "Customer Segmentation"
+])
+
 
 # Top Customers
 if option == "Top Customers":
@@ -39,3 +44,43 @@ elif option == "Daily Sales":
             st.error("Failed to fetch daily sales data")
     else:
         st.warning("End date must be after start date")
+        
+elif option == "Customer Segmentation":
+    st.header("Customer Segmentation")
+    
+    customer_id = st.number_input("Enter Customer ID", min_value=0, step=1)
+    
+    if st.button("Get Segment"):
+        try:
+            st.write("Sending request to API...")
+            st.write(f"Customer ID: {customer_id}")
+
+            response = requests.get(f"{API_URL}/customer-segment", params={"customer_id": customer_id})
+            st.write("Response received")
+            st.write("Raw JSON:", response.json())
+
+            if response.status_code == 200:
+                result = response.json()
+                if "error" in result:
+                    st.error(result["error"])
+                else:
+                    st.write("Parsed result:", result)
+                    segment_labels = {
+                        0: "Loyal",
+                        1: "At Risk",
+                        2: "High Value",
+                        3: "New"
+                    }
+                    segment_name = segment_labels.get(result["segment"], "Unknown")
+                    
+                    st.success(f"Segment: {segment_name}")
+                    st.write(f"Recency: {result['recency']} days")
+                    st.write(f"Frequency: {result['frequency']} purchases")
+                    st.write(f"Monetary Value: ${result['monetary']}")
+                    
+            else:
+                st.error("Failed to fetch data from API")
+                st.write(" Response code:", response.status_code)
+
+        except Exception as e:
+            st.error(f"Error: {e}")
